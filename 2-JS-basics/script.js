cdfaa05facb6e649caed1bb7570d662d4081d93f
Michael Oluade <Michaeloluade@hotmail.co.uk>
2019-06-09 15:39:08 +0100

    Variables and data types

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -1,1 +1,17 @@
-console.log('Hello World!!!');
+var firstName = 'Michael';
+console.log(firstName);
+
+var lastName = 'Oluade'
+var age = 25;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'QA Engineer'
+console.log(job);
+
+var $5years =5;
+console.log($5years);
